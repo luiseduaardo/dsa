22dc2716a6522a7c7ae782754fe7d06dd930bf01
Luís Eduardo <cavalcantesantosluiseduardo@gmail.com>
2025-09-10 21:18:49 -0300

    initial commit

---
+++ cpp/aula15.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n = 0;
+
+    while(n < 10) {
+        cout << n << "\n";
+        cin >> n;
+    }
+
+    cout << "\nRotina finalizada\n";
+
+    return 0;
+}
